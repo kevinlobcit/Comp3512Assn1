561fda8a11b2e57ef69c16f906d7bb2e39a0c8bc
Kevin Lo <klo74@my.bcit.ca>
2018-10-11 22:52:01 -0700

    More functions added like overloading ++, == etc

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
+#include "matrix.hpp"
 
 int main() {
 
+    matrix box = matrix(3);
+    box.set_value(2,2,10);
+
+    matrix box2 = matrix(3);
+    box2.set_value(1,2,4);
+    box2.set_value(1,1,4);
 
 
 
 
+    box += box2;
 
 
 
-
-
-
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << box;
     return 0;
 }
