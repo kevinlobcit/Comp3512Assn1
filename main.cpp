0596aa6b2342cc6a6294825dc905166bc052d35d
Kevin Lo <klo74@my.bcit.ca>
2018-10-16 23:55:58 -0700

    made the main print in a better format

--- main.cpp
+++ main.cpp
@@ -10,15 +10,16 @@ int main() {
     //Inputting the array of hth
     matrix G;
     int W = -1;
-    G.filler("connectivity.txt", G, W);
-    std::cout<<G;
+    G.filler("connectivity.txt", G, W); //G is the matrix, and W is the size of the array
+
+    std::cout<< "Base Array\n"<<G<<std::endl;
     pageRank ranker = pageRank();
 
     matrix importance = ranker.getImportance(G, W);
     std::cout << "Importance\n" << importance << std::endl;
 
     matrix M = ranker.randomWalk(importance, 0.85, W);
-    std::cout<<"randomwalk\n" << M <<std::endl;
+    std::cout<<"Randomwalk\n" << M <<std::endl;
 
     matrix rank = matrix(W,1);
     ranker.markov(M, rank, W, 10000);
